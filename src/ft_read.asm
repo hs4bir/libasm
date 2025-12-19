7963fba1a8f6c2a65f4a6ea6ebafdabc8a14bfe6
hs4bir <lenartlola@proton.me>
2025-12-19 07:46:46 +0100

    Add error handling for read/write

--- src/ft_read.asm
+++ src/ft_read.asm
@@ -1,15 +1,30 @@
-; ft_read by 0xdeadabed
-; 19.06.2022 11:00 AM
-; nasm -f elf64 ft_read.asm
+	; ft_read by hs4bir
+	; 19.06.2022
+	; nasm -f elf64 ft_read.asm
 
-; function prototype -> read(int fd, char *buf, size_t count)
-; attempts to read o count from fd into buf.
+	; function prototype -> read(int fd, char *buf, size_t count)
+	; attempts to read o count from fd into buf.
 
-segment	.text
+	segment .text
 
-global	_ft_read
+	global ft_read
+	extern __errno_location
 
-_ft_read:
-	mov		rax, 0x00		; Move 0x00 which is the number of read systemcall in the syscall table structure ro rax
-	syscall					; Do the syscall
+ft_read:
+	;   Move 0x00 the number of read systemcall in the syscall table structure to rax
+	mov rax, 0x00
+	;   Invoke syscall
+	syscall
+	ret
+
+	;   Check for error
+	cmp rax, 0
+	jl  _error
+
+_error:
+	neg  rax
+	mov  rdi, rax
+	call __errno_location
+	mov  [rax], rdi
+	mov  rax, -1
 	ret
--- src/ft_write.asm
+++ src/ft_write.asm
@@ -1,16 +1,35 @@
-; ft_write by 0xdeadabed
-; 19.06.2022 10:12 AM
-; nasm -f elf64 ft_strlen.asm
+	; ft_write by hs4bir
+	; 19.06.2022
+	; nasm -f elf64 ft_strlen.asm
 
-; function prototype -> write(int fd, const char *buf, size_t nbyte)
-; writes nbyte from buf on the specified fd.
+	; function prototype -> write(int fd, const char *buf, size_t nbyte)
+	; writes nbyte from buf on the specified fd.
 
-segment	.text
+	segment .text
 
-global	_ft_write
+	extern __errno_location
+	global ft_write
 
-_ft_write:
-	mov		rax, 0x01		; Call the write system-call, its number in the systemcall table structure is 0x01 for x64 arch
+ft_write:
+	;   Call the write system-call, its number in the systemcall table structure is 0x01 for x64 arch
+	mov rax, 0x01
 	syscall
 	ret
-	
+
+	;   Check for err
+	cmp rax, 0
+	jl  _error
+	ret
+
+_error:
+	;    Make error code positive
+	neg  rax
+	;    Save error code
+	mov  rdi, rax
+	;    Get errno address. Note, we should call ___error on macOS
+	call __errno_location
+	;    store error code at errno location
+	mov  [rax], rdi
+	mov  rax, -1
+	ret
+
