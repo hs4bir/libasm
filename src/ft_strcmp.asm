73891982ad2af56ab5f5bb214ff18e81ede1d892
hs4bir <lenartlola@proton.me>
2025-12-19 07:47:24 +0100

    Simplify strcmp

--- src/ft_strcmp.asm
+++ src/ft_strcmp.asm
@@ -1,40 +1,42 @@
-; ft_strcmp by 0xdeadabed
-; 19.06.2022 06:52 PM
-; nasm -f elf64 ft_strcmp.asm
+	; ft_strcmp by hs4bir
+	; 19.06.2022
+	; nasm -f elf64 ft_strcmp.asm
 
-; function prototype -> strcmp(char *s1, char *s2)
-; returns 0 if the strings are equal
-; rdi register -> the first argument
-; rsi register -> the second argument
+	; function prototype -> strcmp(char *s1, char *s2)
+	; returns 0 if the strings are equal
+	; rdi register -> the first argument
+	; rsi register -> the second argument
 
-segment .text						; the text segment or section is dedicated to the actual code - aka code segment.
+	segment .text; the text segment or section is dedicated to the actual code - aka code segment.
 
-global	_ft_strcmp					; global tells the kernel where the program starts
+	global ft_strcmp
 
-_ft_strcmp:
-	push	rcx
-	xor		rcx, rcx
-	jmp		_loop_cmp
+ft_strcmp:
+	;   index = 0
+	xor rcx, rcx
 
 _loop_cmp:
-	cmp		[rsi+rcx], byte 0x0
-	jz		_return_zero
-	cmp		[rdi+rcx], byte 0x0
-	jz		_return_zero
-	mov		al, [rsi+rcx]			; Put a byte from rsi to al
-	mov		bl, [rdi+rcx]			; Put a byte from rdi to bl
-	cmp		al, bl					; Compare the two bytes
-	jne		_return_one				; Jump to the function that returns one if they are not equal
-	inc		rcx
-	jmp		_loop_cmp
+	;     Load s1[i] and d2[i] (zero-extended)
+	movzx rax, byte[rdi + rcx]
+	movzx rdx, byte[rsi + rcx]
 
+	;   Compare characters
+	cmp al, dl
+	jne _diff
 
-_return_one:
-	pop		rcx
-	mov		rax, 1
+	;    Check null terminator
+	test al, al
+	jz   _equal
+
+	inc rcx
+	jmp _loop_cmp
+
+_diff:
+	;   s1[i] - s2[i]
+	sub rax, rdx
 	ret
 
-_return_zero:
-	pop		rcx
-	mov		rax, 0
+_equal:
+	xor rax, rax
 	ret
+
